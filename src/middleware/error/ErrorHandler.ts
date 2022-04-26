314358382c36a6ed14583bf2ea9b2c46de40e5cb
Komeil Ghasemi <kgh1374@gmail.com>
2022-04-26 18:06:23 +0430

    v2

--- src/middleware/error/ErrorHandler.ts
+++ src/middleware/error/ErrorHandler.ts
@@ -1,7 +1,7 @@
 import { ErrorRequestHandler , RequestHandler } from "express";
 
 export class ErrorHandler{
-    static internal : ErrorRequestHandler = async (err, req, res, next) => {
+    static internal : ErrorRequestHandler = (err, req, res, next) => {
         console.log('error ->', err)
     
         try {
@@ -14,7 +14,7 @@ export class ErrorHandler{
         res.status(500).json("internal Error")
     };
 
-    static notFound : RequestHandler = async (req, res, next) => {
+    static notFound : RequestHandler = (req, res, next) => {
         res.status(404).json("Whats you looking for ??? check url again , please")
     };
 }
